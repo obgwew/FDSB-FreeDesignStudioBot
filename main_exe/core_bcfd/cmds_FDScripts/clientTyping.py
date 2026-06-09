e726ceecdea468e83f23621c48aef566b040c3a6
obg uwu <177462148+obgwew@users.noreply.github.com>
2026-06-09 12:46:35 +0300

    Delete main_exe directory
    Signed-off-by: obg uwu <177462148+obgwew@users.noreply.github.com>

--- main_exe/core_bcfd/cmds_FDScripts/clientTyping.py
+++
@@ -1,8 +0,0 @@
-# cmds_FDScripts/clientTyping.py
-import discord
-from FDScript import ExecutionContext, Command
-
-
-async def execute(cmd: Command, args: list[str], ctx: ExecutionContext, ch: discord.abc.Messageable) -> None:
-    ctx.start_typing(ch)
-    ctx.log_event("clientTyping → started")
