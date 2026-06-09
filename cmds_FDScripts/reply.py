3f931f9da6af39429cfde0b23549875a2fa81d6d
obg uwu <177462148+obgwew@users.noreply.github.com>
2026-06-09 12:44:03 +0300

    Delete cmds_FDScripts directory
    Signed-off-by: obg uwu <177462148+obgwew@users.noreply.github.com>

--- cmds_FDScripts/reply.py
+++
@@ -1,7 +0,0 @@
-# cmds_FDScripts/reply.py
-import discord
-from FDScript import ExecutionContext, Command
-
-async def execute(cmd: Command, args: list[str], ctx: ExecutionContext, ch: discord.abc.Messageable) -> None:
-    ctx.is_global_reply = True
-    ctx.log_event("reply → reply mode enabled")
